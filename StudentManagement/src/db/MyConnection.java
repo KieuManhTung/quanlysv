43514f9f5b898793f630eed28a66373ab919f019
quang10122003 <qdao10122003badao@gmail.com>
2023-04-24 16:42:03 +0700

    quang

---
+++ StudentManagement/src/db/MyConnection.java
@@ -0,0 +1,29 @@
+
+package db;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import student.Student;
+
+public class MyConnection {
+   public static Connection getJDBConnection(){
+       String url = "jdbc:mysql://localhost:3306/student_management";
+       String user ="root";
+       String password ="quangcuon2k3";
+       try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           return DriverManager.getConnection(url, user, password);
+       } catch (ClassNotFoundException ex) {
+           Logger.getLogger(MyConnection.class.getName()).log(Level.SEVERE, null, ex);
+       } catch (SQLException ex) {
+           Logger.getLogger(MyConnection.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       
+       return null;
+       
+   }
+   
+       
+}
