971a7541ee67138d7b17c7b5ad0b64dd5bfb4e0b
tung <21012099@st.phenikaa-uni.edu.vn>
2023-05-25 02:41:04 +0700

    tung

--- StudentManagement/src/db/MyConnection.java
+++ StudentManagement/src/db/MyConnection.java
@@ -1,14 +1,20 @@
 
 package db;
+import com.mysql.cj.xdevapi.Statement;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import student.Student;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+import student.Account;
 // kết nối mysql 
 public class MyConnection {
-   public static Connection getJDBConnection(){
+    public  Connection getJDBConnection(){
        String url = "jdbc:mysql://localhost:3306/student_management";
        String user ="root";
        String password ="quangcuon2k3";
@@ -22,6 +28,24 @@ public class MyConnection {
        }
        
        return null;
-       
-   }    
+   }   
+    public boolean loginSv(String username ,String password){
+        Connection conn = getJDBConnection();
+        String sql ="SELECT * FROM tksv WHERE username = ? and id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MyConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
 }
